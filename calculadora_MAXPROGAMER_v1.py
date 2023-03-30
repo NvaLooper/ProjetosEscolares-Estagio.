e3e42127f6044058e57ba3def4320d7ce347a1cc
Vitor Silvares (NvaLooper) <99556745+NvaLooper@users.noreply.github.com>
2023-03-30 22:59:31 +0100

    Update calculadora_MAXPROGAMER_v1.py

--- calculadora_MAXPROGAMER_v1.py
+++ calculadora_MAXPROGAMER_v1.py
@@ -13,30 +13,42 @@ while True:
     else:
         
         print("Escolha o tipo de conta:\n 1- Divisão\n 2- Multiplicação\n 3- Subtração\n 4- Soma\n\n")
-        TDC = int(input("Você escolheu a operação número: ")) #Tipo de conta
-        print("")
 
-        print("\n","Você não escolheu uma operação válida, tente novamente.","\n")
+        while True:    
+            try:
+                TDC = float(input("Você escolheu a operação número: ")) #Tipo de conta
+                print("")
 
-        if TDC == 1:
-            while b==0:
-             print("Escreva um segundo número novamente,\npelas regras matemáticas 0 não pode\nser o denominador de nenhuma conta de\ndivisão.\n")
-             b=int(input())
-            print(a,"/",b,"=", a/b)     
+            except ValueError:
+                print("\n","------- Você não escolheu uma operação válida, tente novamente -------","\n")
 
-        elif TDC == 2:
-            print(a,"x",b,"=", a*b)
+            else:
+                if TDC == 1:
+                    while b==0:
+                        print("Escreva um segundo número novamente,\npelas regras matemáticas 0 não pode\nser o denominador de nenhuma conta de\ndivisão.\n")
+                        b=int(input())
+                    print(a,"/",b,"=", a/b)  
+                    break
 
-        elif TDC == 3:
-            print(a,"-",b,"=", a-b)
+                elif TDC == 2:
+                    print(a,"x",b,"=", a*b)
+                    break 
 
-        elif TDC == 4:
-            print(a,"+",b,"=", a+b)
+                elif TDC == 3:
+                    print(a,"-",b,"=", a-b)
+                    break 
+
+                elif TDC == 4:
+                    print(a,"+",b,"=", a+b)
+                    break
+
+                else:
+                    print("\n","------- Você não escolheu uma operação válida, tente novamente -------","\n")
         
-    parar = input("Se quiser parar a calculadora digite STOP, se não aperte enter.\n")
-    min = parar.lower()
-    if min == "stop" or min == "parar":
-        print("\n","------- Obrigado por usar a calculadora MAXPROGAMER -------","\n")
-        break
-    else:
-        print ("---- Escolha novamente dois números ----\n")
+        parar = input("Se quiser parar a calculadora digite STOP, se não aperte enter.\n")
+        min = parar.lower()
+        if min == "stop" or min == "parar":
+            print("\n","------- Obrigado por usar a calculadora MAXPROGAMER -------","\n")
+            break
+        else:
+            print ("---- Escolha novamente dois números ----\n")
