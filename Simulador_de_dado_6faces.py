3fc277f9e02a6182bf7d5b016d98b8abf2f9b95d
NvaLooper <99556745+NvaLooper@users.noreply.github.com>
2023-03-30 14:15:39 +0000

    Update Simulador_de_dado_6faces.py

--- Simulador_de_dado_6faces.py
+++ Simulador_de_dado_6faces.py
@@ -1,17 +1,24 @@
 #Simulação de um dado de 6 faces
 while True:
     Resposta = input("Você gostaria de girar o dado? ")
-    if Resposta == "Sim" or Resposta == "sim" or Resposta == "S":
+    if Resposta == "Sim" or Resposta == "sim" or Resposta == "S" or Resposta == "s":
          print("")
          break   
     else:
-       print("\n","Ok, quando quiser girar escreva que sim","\n")
+       print("\n","Quando quiser girar o dado escreva que sim(s)","\n")
+    
+from random import randint
+print("O número gerado foi:",randint(1,6),"\n")
 
 while True:
-    from random import randint
-    print("O número gerado foi:",randint(1,6),"\n")
+    
     GirarNov = input("Você quer gerar um número novamente? ")
-    if GirarNov == "Sim" or GirarNov == "sim" or GirarNov == "S":
-        print("")
-    else:
+    if GirarNov == "Sim" or GirarNov == "sim" or GirarNov == "S" or GirarNov == "s":
+        from random import randint
+        print("\n","O número gerado foi:",randint(1,6),"\n")
+        
+    elif GirarNov == "Não" or GirarNov == "não" or GirarNov == "N" or GirarNov == "n": 
+        print("\n","------- Obrigado por jogar :) -------","\n")
         break
+    else:
+        print("\n","------- Escreva novamente com sim(s) ou não(n) -------","\n")
