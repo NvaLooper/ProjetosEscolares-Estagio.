2496b251bb67e2e87b6b924a50c234bee429ae15
NvaLooper <99556745+NvaLooper@users.noreply.github.com>
2023-03-30 14:18:48 +0000

    Update calculadora_MAXPROGAMER_v1.py

--- calculadora_MAXPROGAMER_v1.py
+++ calculadora_MAXPROGAMER_v1.py
@@ -1,6 +1,6 @@
 #"\n" serve para pular uma linha e pode ser usado repetidamente.
 print("CALCULATOR MAXPROGAMER\n")
-print("Escolha dois números\n")
+print("------- Escolha dois números -------\n")
 while True:
     a=float(input("Primeiro número: "))
     print("")
@@ -11,25 +11,28 @@ while True:
     
     TDC = int(input("Você escolheu a operação número: ")) #Tipo de conta
     print("")
-    
-    if TDC == 1:
-        while b==0:
-            print("Escreva um segundo número novamente,\npelas regras matemáticas 0 não pode\nser o denominador de nenhuma conta de\ndivisão.\n")
-            b=int(input())
-        print(a,"/",b,"=", a/b)
+
+    try:
+        if TDC == 1:
+            while b==0:
+                print("Escreva um segundo número novamente,\npelas regras matemáticas 0 não pode\nser o denominador de nenhuma conta de\ndivisão.\n")
+                b=int(input())
+            print(a,"/",b,"=", a/b)
         
-    elif TDC == 2:
-        print(a,"x",b,"=", a*b)
+        elif TDC == 2:
+            print(a,"x",b,"=", a*b)
 
-    elif TDC == 3:
-        print(a,"-",b,"=", a-b)
+        elif TDC == 3:
+            print(a,"-",b,"=", a-b)
 
-    else:
-        print(a,"+",b,"=", a+b)
+        elif TDC == 4:
+            print(a,"+",b,"=", a+b)
+    except:
+        print("Operação escolhida invalida")
 
     parar = input("Se quiser parar a calculadora digite STOP, se não aperte enter.\n")
-    if parar == "STOP":
+    if parar == "STOP" or parar == "stop" or parar == "parar" or parar == "Parar":
+        print("\n","------- Obrigado por usar a calculadora MAXPROGAMER -------","\n")
         break
     else:
         print ("Escolha novamente dois números:\n")
-        
