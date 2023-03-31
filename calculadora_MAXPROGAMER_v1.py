e7d66cf8757876ec33e1c8cfca99e1a2a57e84d0
Vitor Silvares (NvaLooper) <99556745+NvaLooper@users.noreply.github.com>
2023-03-31 09:26:24 +0000

    Update calculadora_MAXPROGAMER_v1.py

--- calculadora_MAXPROGAMER_v1.py
+++ calculadora_MAXPROGAMER_v1.py
@@ -1,4 +1,12 @@
-#"\n" serve para pular uma linha e pode ser usado repetidamente.
+#Intruções das funções utilizadas:
+
+#while - Como no inglês representa enquanto, serve para criar um loop. Se você quiser parar o loop precisa quebra-lo com a variável Break
+#"\n"- Serve para pular uma linha e pode ser usado repetidamente.
+#pow - Serve para colocar um número em expoente, o primeiro número é a base e o segundo é o expoente.
+#try - Ele tenta fazer o que foi pedido, se não conseguir ele faz volta e tenta novamente. Junto com a função except e finally.
+#except - Junto com a função try, ele espera um tipo de erro, se esse erro acontecer ele faz o que você digitar depois dos dois pontos.
+#.lower - Serve para deixar todas as palavras que estão na variável ficarem como letra minúscula. (.upper faz o oposto.)
+
 print("CALCULATOR MAXPROGAMER\n")
 print("---- Escolha dois números ----\n")
 while True:
@@ -12,20 +20,20 @@ while True:
     
     else:
         
-        print("Escolha o tipo de conta:\n 1- Divisão\n 2- Multiplicação\n 3- Subtração\n 4- Soma\n\n")
+        print("Escolha o tipo de conta:\n 1- Divisão\n 2- Multiplicação\n 3- Subtração\n 4- Soma\n 5- Potenciação\n 6- Radiciação  \n\n")
 
         while True:    
             try:
                 TDC = float(input("Você escolheu a operação número: ")) #Tipo de conta
                 print("")
 
-            except ValueError:
+            except ValueError: 
                 print("\n","------- Você não escolheu uma operação válida, tente novamente -------","\n")
 
             else:
                 if TDC == 1:
                     while b==0:
-                        print("Escreva um segundo número novamente,\npelas regras matemáticas 0 não pode\nser o denominador de nenhuma conta de\ndivisão.\n")
+                        print("Escreva um segundo número novamente, pelas regras matemáticas 0\nnão pode ser o denominador de nenhuma conta de divisão.\n")
                         b=int(input())
                     print(a,"/",b,"=", a/b)  
                     break
@@ -40,6 +48,15 @@ while True:
 
                 elif TDC == 4:
                     print(a,"+",b,"=", a+b)
+                    break
+
+                elif TDC == 5:
+                    print(a,"^",b,"=", pow(a,b)) 
+                    break
+
+                elif TDC == 6:
+                    print("O primeiro número refere ao índice da raiz e o segundo é o valor que será racionalizado")
+                    print(b,"√",a,"=", pow(a, 1/b))
                     break
 
                 else:
